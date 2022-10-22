db11b8455fdfa358fdadfce4028763fa707f633d
Aurore <aurore.martin504@gmail.com>
2022-10-22 16:28:02 +0200

    html-nav

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,37 @@
 const path = require('path');
 
 module.exports = {
-    mode: "development",
-    entry: path.resolve(__dirname, './src/index.js'),
-    devtool: "eval-source-map",
-    module: {
-        rules: [
-          {
-             test: /\.(js|jsx)$/,
-             exclude: /node_modules/,
-             use: ['babel-loader']
-          },
-          {
-             test: /\.(s(a|c)ss)$/,
-             use: ['style-loader','css-loader', 'sass-loader']
-          },
-	  {
-		test: /\.(png|svg|jpg|jpeg|gif)$/i,
-		type: 'asset/resource',
-	  },
-        ],
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js',
-    },
-    devServer: {
-	static: {
-		directory: path.join(__dirname, 'dist'),
+	mode: 'none',
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
 	},
-	compress: true,
-	port: 8080,
-  },
+
+	module: {
+		rules: [
+			{
+			test: /\.s[ac]ss$/i,
+				use: [
+				"style-loader",
+				"css-loader",
+				{
+					loader: "sass-loader",
+					options: {
+					// Prefer `dart-sass`
+					implementation: require("sass"),
+					},
+				},
+				],
+			},
+		],
+	},
+
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'dist'),
+		},
+		compress: true,
+		port: 8080,
+	},
 };
