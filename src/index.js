97c328c8b8041f7e8f13acf371ef2bb251a1bc01
Aurore <aurore.martin504@gmail.com>
2022-10-25 10:53:44 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,53 +1,41 @@
 import "./index.scss";
 
-import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
-library.add(fas) 
+library.add(fas);
 
-dom.i2svg() 
-
-
-
+dom.i2svg();
 
 // add event listener to detect scroll
 //target element to apply parallax effect to
 //determine amount of pixel to determine the rate at which it will scroll
 
+const nav = document.querySelector(".nav");
+const target = document.querySelectorAll(".scroll");
 
-  const nav = document.querySelector('.nav')
-  const target = document.querySelectorAll('.scroll');
+window.addEventListener("scroll", (event) => {
+  var top = window.pageYOffset;
 
-  window.addEventListener('scroll', (event) => {
-    var top = window.scrollY;
-  
+  if (top >= 100) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
 
-    if (top >= 100){
-      nav.classList.add('active')
+  var index = 0,
+    length = target.length;
+  for (index; index < length; index++) {
+    var pos = window.pageYOffset * target[index].dataset.rate;
+
+    if (target[index].dataset.direction === "vertical") {
+      target[index].style.transform = "translate3d(0px," + pos + "px, 0px)";
     } else {
-      nav.classList.remove('active');
+      var posX = window.pageYOffset * target[index].dataset.ratex;
+      var posY = window.pageYOffset * target[index].dataset.ratey;
+
+      target[index].style.transform =
+        "translate3d(" + posX + "px, " + posY + "px, 0px)";
     }
-
-    var index = 0, length = target.length;
-    for (index; index < length; index++) {
-        var pos = window.pageYOffset * target[index].dataset.rate;
-
-        if(target[index].dataset.direction === 'vertical') {
-            target[index].style.transform = 'translate3d(0px,'+pos+'px, 0px)';
-        } else {
-            var posX = window.pageYOffset * target[index].dataset.ratex;
-            var posY = window.pageYOffset * target[index].dataset.ratey;
-            
-            target[index].style.transform = 'translate3d('+posX+'px, '+posY+'px, 0px)';
-        }
-    }
-
-
-
-  })
-
-
-
-
-
-
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -38,20 +38,7 @@ module.exports = {
 				    presets: ['@babel/preset-env']
 				  }
 				}
-			},
-			{
-				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-				use: [
-				  {
-				    loader: 'file-loader',
-				    options: {
-				      name: '[name].[ext]',
-				      outputPath: 'fonts/'
-				    }
-				  }
-				]
-			},
-			
+			},		
 		],
 	},
 
